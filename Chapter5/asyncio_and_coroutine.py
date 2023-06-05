a21739eef0c7b2d45d795c7d29c14585dbaf96f2
christyuda <yudacihuy09@gmail.com>
2023-06-05 23:38:06 +0700

    tambahan chapter 4 dan 5

---
+++ Chapter5/asyncio_and_coroutine.py
@@ -0,0 +1,69 @@
+import asyncio
+import time
+from random import randint
+
+async def penjadwalan_kereta():
+    print('Memulai penjadwalan kereta api\n')
+    input_value = randint(0, 1)
+    await asyncio.sleep(1)
+
+    if input_value == 0:
+        result = await jadwal_perjalanan_1(input_value)
+    else:
+        result = await jadwal_perjalanan_2(input_value)
+
+    print('Proses Transisi:\nMemulai penjadwalan kereta api memanggil ' + result)
+
+
+async def jadwal_perjalanan_1(transition_value):
+    output_value = 'Jadwal Perjalanan 1 dengan nilai transisi = %s\n' % transition_value
+    input_value = randint(0, 1)
+    await asyncio.sleep(1)
+
+    print('...evaluasi...')
+    if input_value == 0:
+        result = await jadwal_perjalanan_3(input_value)
+    else:
+        result = await jadwal_perjalanan_2(input_value)
+
+    return output_value + 'Jadwal Perjalanan 1 memanggil %s' % result
+
+
+async def jadwal_perjalanan_2(transition_value):
+    output_value = 'Jadwal Perjalanan 2 dengan nilai transisi = %s\n' % transition_value
+    input_value = randint(0, 1)
+    await asyncio.sleep(1)
+
+    print('...evaluasi...')
+    if input_value == 0:
+        result = await jadwal_perjalanan_1(input_value)
+    else:
+        result = await jadwal_perjalanan_3(input_value)
+
+    return output_value + 'Jadwal Perjalanan 2 memanggil %s' % result
+
+
+async def jadwal_perjalanan_3(transition_value):
+    output_value = 'Jadwal Perjalanan 3 dengan nilai transisi = %s\n' % transition_value
+    input_value = randint(0, 1)
+    await asyncio.sleep(1)
+
+    print('...evaluasi...')
+    if input_value == 0:
+        result = await jadwal_perjalanan_1(input_value)
+    else:
+        result = await end_state(input_value)
+
+    return output_value + 'Jadwal Perjalanan 3 memanggil %s' % result
+
+
+async def end_state(transition_value):
+    output_value = 'End State dengan nilai transisi = %s\n' % transition_value
+    print('...menghentikan komputasi...')
+    return output_value
+
+
+if __name__ == '__main__':
+    print('Simulasi Sistem Penjadwalan Kereta API menggunakan Asyncio Coroutine')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(penjadwalan_kereta())
---
+++ Chapter5/asyncio_loop.py
@@ -0,0 +1,30 @@
+import asyncio
+import time
+import random
+
+async def jadwal_perjalanan(stasiun):
+    print(f"Memproses jadwal perjalanan kereta api di stasiun {stasiun}")
+    await asyncio.sleep(random.randint(0, 5))
+    print(f"Jadwal perjalanan kereta api di stasiun {stasiun} selesai")
+
+async def penjadwalan_kereta(stasiun, end_time, loop):
+    await jadwal_perjalanan(stasiun)
+    if (loop.time() + 1.0) < end_time:
+        loop.call_later(1, penjadwalan_kereta, stasiun, end_time, loop)
+    else:
+        loop.stop()
+
+async def main():
+    loop = asyncio.get_event_loop()
+    end_loop = loop.time() + 60
+
+    stasiun_kereta = ['Gambir', 'Bandung', 'Yogyakarta', 'Surabaya', 'Malang']
+    tasks = []
+    for stasiun in stasiun_kereta:
+        task = asyncio.create_task(penjadwalan_kereta(stasiun, end_loop, loop))
+        tasks.append(task)
+    
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    asyncio.run(main())
